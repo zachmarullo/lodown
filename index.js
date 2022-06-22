8bb3eddfc6ae10ac1971a5a2d84473deb3874499
zachmarullo <zachmarullo@gmail.com>
2022-06-22 17:29:09 +0000

    Lodown 6/22/22

--- index.js
+++ index.js
@@ -32,3 +32,76 @@ module.exports.each = each;
 function identity(value){
     return value;
 };
+
+/**
+ * typeOf: function takes in a value and returns the type of value
+ * @param {any value} value: Function takes in any value
+ * @returns {typeOf value}: Function returns the type of any value plugged in as a string 
+ */
+
+function typeOf(value){
+    if (typeof value === 'string'){
+        return 'string';
+    }else if (typeof value === 'object' && Array.isArray(value) === true){
+        return 'array';
+    } else if (typeof value === 'object' && Array.isArray(value) === false && value !== null && value instanceof Date === false){
+        return 'object';
+    } else if (typeof value === 'undefined'){
+        return 'undefined';
+    } else if (typeof value === 'number'){
+        return 'number';
+    } else if (typeof value === 'boolean'){
+        return 'boolean';
+    } else if (typeof value === 'object' && value === null){
+        return 'null';
+    } else if (typeof value === 'function'){
+        return 'function';
+    }
+}
+
+/**
+ * first: Function takes in an array and a number and returns either an empty array, an array of <number> elements
+ * starting from the beginning of the array, the whole array, or just the first element.
+ * @param {Array, number}: Function takes in array and number, or just array with no number argument
+ * @returns {Array}: Function returns an array with specified <number> of elements
+ */
+
+function first(array, num){
+    if (!Array.isArray(array)){
+        return [];
+    }
+    if (isNaN(num)){
+        return array[0];
+    }
+    if (num < 0){
+        return [];
+    }
+    if (num > array.length){
+        return array;
+    }
+    return array.slice(0, num)
+}
+
+/**
+ * last: Function is opposite of "first". Function takes in a number and array and returns an empty array, an array with
+ * a certain specified <number> of results starting from the back of the array, just the last element of given array,
+ * or the whole array. 
+ * @param{array, number}: Function takes in an <array> and a <number> or just an array if no number is given or isNaN
+ * @returns{array}: Function returns a new array depending on <number> argument plugged in.
+ */
+
+function last(array, number){
+    if(!Array.isArray(array)){
+        return [];
+    }
+    if(isNaN(number)){
+        return array[array.length - 1];
+    }
+    if (number < 0){
+        return [];
+    }
+    if (number > array.length){
+        return array;
+    }
+    return array.slice(number - 1, array.length)
+}
